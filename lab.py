de96f21b39376e1e571e21eefbff5d99324901eb
atippe63 <153721457+atippe63@users.noreply.github.com>
2024-02-27 11:46:37 +0700

    เพิ่ม class

--- lab.py
+++ lab.py
@@ -46,3 +46,19 @@ for row in ข้อมูลทั้งหมด:
 
 conn.commit()
 conn.close()
+
+class MyClass:
+    def __init__(self):
+        self._x = None
+
+    def getx(self):
+        return self._x
+
+    def setx(self, value):
+        self._x = value
+
+    def delx(self):
+        del self._x
+
+    x = property(getx, setx, delx, "I'm the 'x' property.")
+
